b6ae50b5745b8d99e4b090265a741c97a5eb61a3
A7medCSN <nos107n@gmail.com>
2024-09-23 11:43:41 +0400

    original

---
+++ CountOccurrencesOfSubstring.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class CountOccurrencesOfSubstring {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("enter a phrase:");
+        String phrase = scan.nextLine();
+        System.out.println("enter a sub-phrase to find from original phrase:");
+        String subStringTOFind = scan.nextLine();
+        countOccurrences(phrase, subStringTOFind);
+
+    }
+
+    public static int countOccurrences(String str, String substr) {
+
+        int occurrenceOfSubstring = 0;
+        String[] phraseWords = str.split(" ");
+
+        for (String s : phraseWords) {
+           if ( str.contains(substr) == true ){
+               occurrenceOfSubstring++;
+           }
+        }
+        System.out.println("occurrence= " + occurrenceOfSubstring);
+        return occurrenceOfSubstring;
+    }
+}
