46c74ddc35cf6833815e481e2d607cf5929d8113
A7medCSN <nos107n@gmail.com>
2024-09-24 16:10:49 +0400

    checking the size of input string

--- SubstringBeginAndEnd.java
+++ SubstringBeginAndEnd.java
@@ -4,18 +4,25 @@ public class SubstringBeginAndEnd {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-        System.out.println("enter a string (at least 2 letters):");
+        System.out.println("enter a string:");
         String str = scan.nextLine();
         without2(str);
     }
 
     public static void without2(String str) {
+
+        Scanner scan = new Scanner(System.in);
         String newStr;
-        if (str.substring(0, 2).equals(str.substring(str.length() - 2, str.length()))) {
-            newStr = str.substring(2, str.length());
+
+        while (str.length() < 2) {
+            System.out.println("enter string longer than one character: ");
+            str = scan.nextLine();
+        }
+        if (str.substring(0, 2).equals(str.substring(str.length() - 2))) {
+            newStr = str.substring(2);
             System.out.println(newStr);
         } else if (str.length() == 2) {
-            System.out.println("");
+            System.out.println();
         } else {
             System.out.println(str);
         }
