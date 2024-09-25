dd9ef90664a1694a802bd0984e9ec6b56c99f639
A7medCSN <nos107n@gmail.com>
2024-09-25 11:39:19 +0400

    original code

--- FrontPiece.java
+++ FrontPiece.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
 public class FrontPiece {
+    public static void main(String[] args) {
+
+        int[] firstArray = {3,7};
+        firstTwoElementsMakesNewArray(firstArray);
+    }
+
+    public static void firstTwoElementsMakesNewArray(int[] firstArray) {
+
+        int[] newArray = new int[2];
+        if (firstArray.length > 1) {
+            newArray = new int[]{firstArray[0], firstArray[1]};
+        } else {
+            newArray = new int[]{firstArray[0]};
+        }
+        System.out.println(Arrays.toString(newArray));
+    }
 }
