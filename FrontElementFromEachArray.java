71d17a5bdb4ec992dbbbce16565af0ebdd0f230c
A7medCSN <nos107n@gmail.com>
2024-09-25 11:31:30 +0400

    small modefication

--- FrontElementFromEachArray.java
+++ FrontElementFromEachArray.java
@@ -3,23 +3,21 @@ import java.util.Arrays;
 public class FrontElementFromEachArray {
     public static void main(String[] args) {
 
-        int[] firstArray = {2, 4, 5, 7};
+        int[] firstArray = {3,4,6};
         int[] secondArray = {7, 5, 3};
         frontElementsMakesNewArray(firstArray, secondArray);
     }
 
     public static void frontElementsMakesNewArray(int[] firstArray, int[] secondArray) {
 
-        int[] newArray;
+        int[] newArray = new int[2];
         if (firstArray.length > 0 && secondArray.length > 0) {
             newArray = new int[]{firstArray[0], secondArray[0]};
-            System.out.println(Arrays.toString(newArray));
         } else if (firstArray.length == 0) {
             newArray = new int[]{secondArray[0]};
-            System.out.println(Arrays.toString(newArray));
         } else if (secondArray.length == 0) {
             newArray = new int[]{firstArray[0]};
-            System.out.println(Arrays.toString(newArray));
         }
+        System.out.println(Arrays.toString(newArray));
     }
 }
