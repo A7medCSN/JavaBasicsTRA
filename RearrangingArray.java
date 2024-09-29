2d78053e0ce71080d52a57097ce26f87e2be1d2d
A7medCSN <nos107n@gmail.com>
2024-09-29 15:44:20 +0400

    original code

---
+++ RearrangingArray.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class RearrangingArray {
+    public static void main(String[] args) {
+
+        Integer[] arrayA = {2, 2, 3, 5, 4, 6, 2, 1, 3, 6, 4};
+        put4NextTo3(arrayA);
+    }
+
+    public static void put4NextTo3(Integer[] arrayA) {
+        Integer tempStore;
+        Integer number4Location = 0;
+        for (int i = 0; i < arrayA.length; i++) {
+            if (arrayA[i] == 3) {
+                while (arrayA[number4Location] != 4) {
+                    number4Location++;
+                }
+                tempStore = arrayA[i + 1];
+                arrayA[i + 1] = 4;
+                arrayA[number4Location] = tempStore;
+                System.out.println(Arrays.toString(arrayA));
+            }
+        }
+    }
+}
