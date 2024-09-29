fa24fb8c6edf6d114cb2780de6add59164c70d6c
A7medCSN <nos107n@gmail.com>
2024-09-29 13:08:10 +0400

    original code

--- SumOfDigitsInString.java
+++ SumOfDigitsInString.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
 public class SumOfDigitsInString {
+    public static void main(String[] args) {
+
+        String str = "fdg6gf3s2";
+        findSumOfDigitsInString(str);
+    }
+
+    public static void findSumOfDigitsInString(String str) {
+        int sum = 0;
+        char[] lettersFromStr = str.toCharArray();
+        for (char c : lettersFromStr) {
+            if (Character.isDigit(c)) {
+                String n = String.valueOf(c);
+                int number = Integer.parseInt(n);
+                sum += number;
+            }
+        }
+        System.out.println(sum);
+    }
 }
