d966805d036e29170c091eaf4a037bcd66158f5a
A7medCSN <nos107n@gmail.com>
2024-10-02 14:57:43 +0400

    user input

--- SummationWithTeenRuleExclusion.java
+++ SummationWithTeenRuleExclusion.java
@@ -1,15 +1,31 @@
 
-import java.util.Arrays;
-import java.util.List;
-
+import java.util.ArrayList;
+import java.util.Scanner;
 public class SummationWithTeenRuleExclusion {
     public static void main(String[] args) {
 
-        List<Integer> numbers = Arrays.asList(18, 3, 16, 19);
+        ArrayList<Integer> numbers = getUserInput();
         summingWithoutTeenNumbers(numbers);
     }
 
-    public static void summingWithoutTeenNumbers(List<Integer> numbers) {
+    public static ArrayList<Integer> getUserInput() {
+        Scanner scan = new Scanner(System.in);
+        ArrayList<Integer> numbers = new ArrayList<>();
+        System.out.println("enter integer elements, enter -1 to stop:");
+        Integer inputForArray = scan.nextInt();
+
+        while (true) {
+            if (inputForArray != -1) {
+                numbers.add(inputForArray);
+                inputForArray = scan.nextInt();
+            } else {
+                break;
+            }
+        }
+        return numbers;
+    }
+
+    public static void summingWithoutTeenNumbers(ArrayList<Integer> numbers) {
         Integer sum = 0;
         for (Integer n : numbers) {
             if (n >= 13 && n <= 19 && (n != 15 && n != 16)) {
