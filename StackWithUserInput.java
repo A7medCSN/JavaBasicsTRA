021f30801d17c080b9f9b8d8c7e3d9ff6a545e67
A7medCSN <nos107n@gmail.com>
2024-10-08 10:51:39 +0400

    original code

---
+++ StackWithUserInput.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class StackWithUserInput {
+    public static void main(String[] args) {
+
+        Stack<Integer> numbers = new Stack<>();
+        Scanner scan = new Scanner(System.in);
+        Integer stackElements;
+        System.out.println("enter stack element, enter -1 to stop:");
+        stackElements = scan.nextInt();
+        while (stackElements != -1) {
+            numbers.push(stackElements);
+            stackElements = scan.nextInt();
+        }
+        System.out.println("your stack is " + numbers);
+    }
+}
