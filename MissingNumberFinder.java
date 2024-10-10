f17a0b91040137009eacd3eeed500cd3f61ad7b0
A7medCSN <nos107n@gmail.com>
2024-10-10 11:24:15 +0400

    original code

---
+++ MissingNumberFinder.java
@@ -0,0 +1,40 @@
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class MissingNumberFinder {
+    public static void main(String[] args) {
+
+        findMissingNumberInSequence();
+    }
+
+    public static void findMissingNumberInSequence() {
+
+        Set<Integer> orderedNumbers = new HashSet<>(List.of(0, 1, 6, 5, 4, 3));
+        System.out.println(orderedNumbers);
+        // this way is giving me 2*n complexity
+        Integer sumOfOrderedNumbers = 0;
+        for (Integer x : orderedNumbers) {
+            sumOfOrderedNumbers += x;
+        }
+        Integer sumOfSequence = 0;
+
+        for (int i = 0; i <= orderedNumbers.size(); i++) {
+            sumOfSequence += i;
+        }
+
+        System.out.println("the missing number is: " + (sumOfSequence - sumOfOrderedNumbers));
+
+            /*
+        this way is giving me n^2 complexity for the code
+            for (int i = 0; i < orderedNumbers.size(); i++) {
+            if (!orderedNumbers.contains(i)) {
+                System.out.println(i);
+            } else {
+                orderedNumbers.remove(i);
+            }
+        }*/
+
+    }
+}
+
