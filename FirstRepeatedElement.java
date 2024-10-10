cc853f10b00476a441658c6ebb1502c6404c2785
A7medCSN <nos107n@gmail.com>
2024-10-10 09:58:31 +0400

    original code

---
+++ FirstRepeatedElement.java
@@ -0,0 +1,25 @@
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class FirstRepeatedElement {
+    public static void main(String[] args) {
+
+        List<Integer> listOfNumbers = List.of(1, 2, 3, 4, 5, 1, 3);
+        findFirstRepeatedNumber(listOfNumbers);
+    }
+
+    public static void findFirstRepeatedNumber(List<Integer> listOfNumbers) {
+
+        Set<Integer> nonRepeatedListOfNumber = new HashSet<>();
+        for (Integer i : listOfNumbers) {
+            if (nonRepeatedListOfNumber.contains(i)) {
+                System.out.println("First repeated number is: " + i);
+                return;
+            }
+            nonRepeatedListOfNumber.add(i);
+        }
+        System.out.println("there is  no repeated number!");
+    }
+}
+
