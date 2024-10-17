179491b4d06501d237077ceb4b44eec03a89c8ea
A7medCSN <nos107n@gmail.com>
2024-10-17 12:15:58 +0400

    original code

--- allPairsWithGivenSum.java
+++ allPairsWithGivenSum.java
@@ -2,7 +2,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-public class DistinctElementsInEveryWindowOfSizeK {
+public class allPairsWithGivenSum {
     public static void main(String[] args) {
 
         findTupleEqualsToTarget();
--- distinctElementsInEveryWindowOfSizeK.java
+++ distinctElementsInEveryWindowOfSizeK.java
@@ -3,7 +3,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-public class AllPairsWithGivenSum {
+public class distinctElementsInEveryWindowOfSizeK {
     public static void main(String[] args) {
 
         findDistinctElementsInGivenWindow();
