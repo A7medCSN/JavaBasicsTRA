3c5105d04144c3f694682a4e77091da0b5708b38
A7medCSN <nos107n@gmail.com>
2024-10-21 11:58:43 +0400

    new key takes longest value

---
+++ KeyTakesLongestValue.java
@@ -0,0 +1,37 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class KeyTakesLongestValue {
+    public static void main(String[] args) {
+
+        Map<String, String> lettersRepresentation = new HashMap<>();
+        lettersRepresentation.put("b", "bb");
+        lettersRepresentation.put("c", "cake");
+        lettersRepresentation.put("a", "aaa");
+        System.out.println("Map before modification: " + lettersRepresentation);
+        getLongestValueForKey(lettersRepresentation);
+    }
+
+    public static void getLongestValueForKey(Map<String, String> lettersRepresentation) {
+
+        if ((lettersRepresentation.containsKey("b") && lettersRepresentation.containsKey("a"))) {
+            if (lettersRepresentation.get("a").length() > lettersRepresentation.get("b").length()) {
+                lettersRepresentation.put("c", lettersRepresentation.get("a"));
+            }
+            if (lettersRepresentation.get("a").length() < lettersRepresentation.get("b").length()) {
+                lettersRepresentation.put("c", lettersRepresentation.get("b"));
+            }
+            if (lettersRepresentation.get("a").length() == lettersRepresentation.get("b").length()) {
+                lettersRepresentation.replace("a", "");
+                lettersRepresentation.replace("b", "");
+            }
+            System.out.println("Map after modification: " + lettersRepresentation);
+
+        } else if ((!lettersRepresentation.containsKey("b") && !lettersRepresentation.containsKey("a"))) {
+            System.out.println("both keys a and b are missing");
+        } else {
+            System.out.println("there is no modification");
+        }
+    }
+}
+
