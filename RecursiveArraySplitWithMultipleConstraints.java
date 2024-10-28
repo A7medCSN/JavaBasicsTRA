a10692a84b87f7041fcf8ae32e534f6247caa8c5
A7medCSN <nos107n@gmail.com>
2024-10-28 14:06:41 +0400

    split array to get equal sum using recursion

---
+++ RecursiveArraySplitWithMultipleConstraints.java
@@ -0,0 +1,31 @@
+import java.util.List;
+
+public class RecursiveArraySplitWithMultipleConstraints {
+
+    public static void main(String[] args) {
+        System.out.println(split53(List.of(2, 4, 2)));
+    }
+
+    public static Boolean split53(List<Integer> nums) {
+
+        return helperToSplitAndSum(0, nums, 0, 0);
+    }
+
+    public static Boolean helperToSplitAndSum(Integer index, List<Integer> nums, Integer sumGroup1, Integer sumGroup2) {
+
+        if (index == nums.size()) {
+            Boolean isSumOfGroupsEqual = (sumGroup1 == sumGroup2);
+            return isSumOfGroupsEqual;
+        }
+        int currentElement = nums.get(index);
+        if (currentElement % 5 == 0) { //group1
+            return helperToSplitAndSum(index + 1, nums, sumGroup1 + currentElement, sumGroup2);
+        } else if (currentElement % 3 == 0) { //group2
+            return helperToSplitAndSum(index + 1, nums, sumGroup1, sumGroup2 + currentElement);
+        } else //remaining numbers
+            return helperToSplitAndSum(index + 1, nums, sumGroup1 + currentElement, sumGroup2)
+                    || helperToSplitAndSum(index + 1, nums, sumGroup1, sumGroup2 + currentElement);
+    }
+
+}
+
