742b209c77b52b15d7cc9b7b6c0b2c37e6e315ff
A7medCSN <nos107n@gmail.com>
2024-10-30 15:00:57 +0400

    handling cases with numbers multiple of both 3 and 5

--- RecursiveArraySplitWithMultipleConstraints.java
+++ RecursiveArraySplitWithMultipleConstraints.java
@@ -3,11 +3,11 @@ import java.util.List;
 public class RecursiveArraySplitWithMultipleConstraints {
 
     public static void main(String[] args) {
-        System.out.println(split53(List.of(2, 4, 2)));
+        System.out.println(split53(List.of(3, 2, 9, 4, 2, 15, 10, 10)));
     }
 
     public static Boolean split53(List<Integer> nums) {
-
+        System.out.println("any number is multiple of both 3 and 5 is ignored");
         return helperToSplitAndSum(0, nums, 0, 0);
     }
 
@@ -18,7 +18,9 @@ public class RecursiveArraySplitWithMultipleConstraints {
             return isSumOfGroupsEqual;
         }
         int currentElement = nums.get(index);
-        if (currentElement % 5 == 0) { //group1
+        if (currentElement % 15 == 0) {//group2
+            return helperToSplitAndSum(index + 1, nums, sumGroup1, sumGroup2);
+        } else if (currentElement % 5 == 0) { //group1
             return helperToSplitAndSum(index + 1, nums, sumGroup1 + currentElement, sumGroup2);
         } else if (currentElement % 3 == 0) { //group2
             return helperToSplitAndSum(index + 1, nums, sumGroup1, sumGroup2 + currentElement);
@@ -26,6 +28,5 @@ public class RecursiveArraySplitWithMultipleConstraints {
             return helperToSplitAndSum(index + 1, nums, sumGroup1 + currentElement, sumGroup2)
                     || helperToSplitAndSum(index + 1, nums, sumGroup1, sumGroup2 + currentElement);
     }
-
 }
 
